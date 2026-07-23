1496cd428b4b229389427dfd0f31fb30c5846b13
wharley <wharleyornelasjs@gmail.com>
2026-07-23 15:04:50 -0300

    feat(plan): export plan summary component

--- apps/desktop/src/features/panel/message-components/index.ts
+++ apps/desktop/src/features/panel/message-components/index.ts
@@ -1,5 +1,6 @@
 export { AssistantMessage } from "./AssistantMessage";
 export { DelegationCard } from "./DelegationCard";
 export { PlanReviewCard } from "./PlanReviewCard";
+export { PlanSummaryCard } from "./PlanSummaryCard";
 export { SystemMessage } from "./SystemMessage";
 export { UserMessage } from "./UserMessage";
